93c11f75f3c4b9922d5a74d0c9c1d7bc92cd765a
Lidiia Diachkovskaia <lyatlasova@gmail.com>
2026-04-28 18:26:24 -0400

    all

---
+++ DiceRollerSwiftUI/DiceView.swift
@@ -0,0 +1,18 @@
+//
+//  DiceView.swift
+//  DiceRollerSwiftUI
+//
+//  Created by Lidiia Diachkovskaia on 4/28/26.
+//
+
+import SwiftUI
+
+struct DiceView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    DiceView()
+}
