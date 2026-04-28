42324aa974652ce0295d676fafc426ee5f8521d9
Lidiia Diachkovskaia <lyatlasova@gmail.com>
2026-04-28 16:50:35 -0400

    Initial Commit

---
+++ DiceRollerSwiftUI/DiceRollerSwiftUIApp.swift
@@ -0,0 +1,17 @@
+//
+//  DiceRollerSwiftUIApp.swift
+//  DiceRollerSwiftUI
+//
+//  Created by Lidiia Diachkovskaia on 4/28/26.
+//
+
+import SwiftUI
+
+@main
+struct DiceRollerSwiftUIApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
