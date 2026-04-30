cf7c0172a634b45cc2777dbad14fa384d0f16271
Lidiia Diachkovskaia <lyatlasova@gmail.com>
2026-04-30 18:24:20 -0400

    change toggle color

--- DiceRollerSwiftUI/Pick_a_PalView.swift
+++ DiceRollerSwiftUI/Pick_a_PalView.swift
@@ -30,50 +30,50 @@ struct Pick_a_PalView: View {
                 .foregroundStyle(.tint)
                 .padding()
             
-                List {
-                    ForEach(names, id: \.description) { name in
-                        Text(name)
-                    }
+            List {
+                ForEach(names, id: \.description) { name in
+                    Text(name)
                 }
-                .clipShape(RoundedRectangle(cornerRadius: 8))
+            }
+            .clipShape(RoundedRectangle(cornerRadius: 8))
             
-                TextField("Add name", text: $nameToAdd)
-                    .autocorrectionDisabled(true)
-                    .onSubmit {
-                        if !nameToAdd.isEmpty { names.append(nameToAdd)
-                            nameToAdd = ""
-                        }
-                    }
-                Divider()
-                
-                Toggle("Remoxe when Picked", isOn: $shouldRemovePickedName)
-                
-                Button { // here we have action that is a imperative code
-                    //pickedName = names.randomElement() ?? "No names yet"
-                    
-                    if let randomName = names.randomElement() { //if let - potentia;;y works with nil values
-                        pickedName = randomName
-                        
-                        if shouldRemovePickedName { //this removes a picked name from  a list
-                            names.removeAll { name in
-                                return (name == randomName)
-                            }
-                        }
-                        
-                    } else {
-                        pickedName = ""
+            TextField("Add name", text: $nameToAdd)
+                .autocorrectionDisabled(true)
+                .onSubmit {
+                    if !nameToAdd.isEmpty { names.append(nameToAdd)
+                        nameToAdd = ""
                     }
                 }
+            Divider()
+            
+            Toggle("Remoxe when Picked", isOn: $shouldRemovePickedName)
+                .tint(shouldRemovePickedName ? .accentColor : .gray)
+            Button { // here we have action that is a imperative code
+                //pickedName = names.randomElement() ?? "No names yet"
+                
+                if let randomName = names.randomElement() { //if let - potentia;;y works with nil values
+                    pickedName = randomName
+                    
+                    if shouldRemovePickedName { //this removes a picked name from  a list
+                        names.removeAll { name in
+                            return (name == randomName)
+                        }
+                    }
+                    
+                } else {
+                    pickedName = ""
+                }
+            }
             label: { //declarative code
                 Text("Pick Random Name")
                     .padding(.vertical, 8)
                     .padding(.horizontal, 16)
             }
-                .padding(.top)
-                .buttonStyle(.borderedProminent)
-                .font(.title2)
-            }
-            .padding()
+            .padding(.top)
+            .buttonStyle(.borderedProminent)
+            .font(.title2)
+        }
+        .padding()
     }
 }
 
